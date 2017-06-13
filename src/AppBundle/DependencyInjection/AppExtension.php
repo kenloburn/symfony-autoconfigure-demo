02f9454338c4fb68793d2fd620163d74f30cb6b8
Ruben Knol <ruben.knol@movingimage.com>
2017-06-13 18:33:42 +0200

    Move configuration to AppBundle

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class AppExtension.
+ *
+ * @author Ruben Knol <ruben.knol@movingimage.com>
+ */
+class AppExtension extends Extension
+{
+    /**
+     * Define our collections as container parameter so that our compiler
+     * pass and bundle classes can use the configuration.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+    }
+}
